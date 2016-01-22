670a079311c23c483fce1856924a7aa0beb0459c
findedward <13675875042@sina.cn>
2016-01-22 16:56:29 +0800

    add BKG
    if mobile  , the bkg is a video
    if mobile ,  the video will not load.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,31 @@
-// console.log('This would be the main JS file.');
+$(document).ready(function(){
+    
+    /* 如果是mobile，就不预加载视频 */
+    (function () {
+    	var useragent  = navigator.userAgent.toLowerCase(),
+    	    isIPad     = useragent.match(/ipad/i) == "ipad",
+    	    isIPhoneOs = useragent.match(/iphone os/i) == "iphone os",
+    	    isMidp     = useragent.match(/midp/i) == "midp",
+    	    isUc7      = useragent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4",
+    	    isUc       = useragent.match(/ucweb/i) == "ucweb",
+    	    isAndroid  = useragent.match(/android/i) == "android",
+    	    isCE       = useragent.match("/windows ce/i") == "windows ce",
+    	    isWM       = useragent.match(/windows mobile/i) == "windows mobile";
+
+    	if (isIPad || isIPhoneOs || isMidp || isUc7 || isUc || isAndroid || isCE || isWM) {
+    		$("#video-bkg-hd").attr({
+    			"autoplay" : false,
+    			"loop"     : false,
+    			"preload"  : "none"
+    		});
+    		return true;
+    	} else {
+    		$("#video-bkg-hd").attr({
+    			"autoplay" : true,
+    			"loop"     : true,
+    			"preload"  : "auto"
+    		});
+    		return true;
+    	}
+    })();
+});
